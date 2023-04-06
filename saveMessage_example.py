94d1c1722976075682a60cbd32d4460eddf934ea
vitormendes33 <vitormendes@ienh.com.br>
2023-04-05 21:48:41 -0300

    adicionado

--- saveMessage_example.py
+++ saveMessage_example.py
@@ -1,8 +1,8 @@
 from cryptographyFramework import *
 
 initializeWrite()
-user = "Fulano"
-password = "1234"
+user = "vitor"
+password = "12345"
 encryptedText = encryptMessage(user, password, "Minha mensagem secreta!")
 saveNewLine(encryptedText)
 encryptedText = encryptMessage(user, password, "Minha segunda mensagem secreta!")
